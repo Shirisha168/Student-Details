f59ea40aad3b36517c4de251b4ca5fa079b7fd3e
Pradeep <shirisharao461@gmail.com>
2021-04-28 16:43:43 -0500

    Initial commit

---
+++ DemoApplication/src/main/java/com/javapoint/DemoApplication/student/StudentController.java
@@ -0,0 +1,26 @@
+package com.javapoint.DemoApplication.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    private final StudentService studentService;
+
+@Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping(value="/")
+    public List<Student> getstudents(){
+  return studentService.getstudents();
+
+    }
+}
